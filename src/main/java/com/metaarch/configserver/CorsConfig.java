840629ed5f12774a974cc519628406c1213afb16
Upendra Kristipati <upendrakristipati@users.noreply.github.com>
2026-08-16 14:14:43 +0530

    Access-Control-Allow-Origin

---
+++ src/main/java/com/metaarch/configserver/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.metaarch.configserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**")
+        .allowedOriginPatterns("*")
+        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .exposedHeaders("Authorization", "Location", "Content-Disposition")
+        .allowCredentials(true)
+        .maxAge(3600);
+  }
+}
